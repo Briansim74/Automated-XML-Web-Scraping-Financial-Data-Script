9edc83cc213879d64ad0ba44678336bec619441c
Brian Sim <98273621+Briansim74@users.noreply.github.com>
2024-10-12 08:58:20 +0800

    Update Security_Wise_Holdings_Query.sql

--- Security_Wise_Holdings_Query.sql
+++ Security_Wise_Holdings_Query.sql
@@ -1,18 +1,20 @@
-DELETE FROM dbo.SECURITY_WISE_HOLDINGS; --delete all in table
+TRUNCATE TABLE dbo.SECURITY_WISE_HOLDINGS; --delete all rows in table
 
 SELECT * FROM dbo.SECURITY_WISE_HOLDINGS -- view table
 
 /* CREATE TABLE SECURITY_WISE_HOLDINGS (
-	Date varchar(50),
-	ISIN varchar(50),
-	Security_Deposition varchar(50),
-	Maturity_Date varchar(50),
-	Trades varchar(50),
-	Volume_Cr varchar(50),
-	Open_Yield varchar(50),
-	High_Yield varchar(50),
-	Low_Yield varchar(50), 
-	Last_Yield varchar(50),
-	Wtd_Avg_Yield varchar(50),
-	Wtd_Avg_Price varchar(50),
+	Date VARCHAR(50),
+	ISIN VARCHAR(50),
+	Security_Deposition VARCHAR(50),
+	Maturity_Date VARCHAR(50),
+	Trades INT,
+	Volume_Cr FLOAT(50),
+	Open_Yield FLOAT(50),
+	High_Yield FLOAT(50),
+	Low_Yield FLOAT(50), 
+	Last_Yield FLOAT(50),
+	Wtd_Avg_Yield FLOAT(50),
+	Wtd_Avg_Price FLOAT(50),
 ); */ --create table
+
+DROP TABLE dbo.SECURITY_WISE_HOLDINGS --delete table
